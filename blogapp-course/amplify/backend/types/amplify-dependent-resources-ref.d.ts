c6b356f061cc504afdc5523c0e827895eedf31cc
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2022-04-14 02:28:56 +0600

    authentication added

--- blogapp-course/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ blogapp-course/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,16 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "blogappcourse8f9746b8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
     }
 }
